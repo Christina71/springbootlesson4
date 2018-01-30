7deaee1f85cb7b29a8a41f4785ab5a1cd9c64bd5
Christina71 <mariachristina71@yahoo.com>
2018-01-30 14:31:23 -0500

    initial commit

---
+++ src/main/java/com/example/springbootlesson4/Springbootlesson4Application.java
@@ -0,0 +1,12 @@
+package com.example.springbootlesson4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootlesson4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootlesson4Application.class, args);
+	}
+}
